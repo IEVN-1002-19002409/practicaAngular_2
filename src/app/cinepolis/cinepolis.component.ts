6939f778232f85835df9eafe91a01966beaedc2e
Juan Garcia <19002409@alumnos.utleon.edu.mx>
2023-10-07 09:43:21 -0600

    JCGC

--- src/app/cinepolis/cinepolis.component.ts
+++ src/app/cinepolis/cinepolis.component.ts
@@ -10,39 +10,35 @@ export class CinepolisComponent implements OnInit {
   numBoletas: number = 0;
   tarjetaCineco: boolean = false;
   precioAPagar: number = 0;
-  maxBoletasCompradas: number = 0;
+  precioPorBoleta: number = 1200.0; // Precio en centavos ($12,000)
+  maxBoletasCompradas: number = 0; // Límite inicial
+  calcularPrecio: boolean = false;
+  descuentoAdicionalTarjeta: number = 0.10; // Descuento adicional por tarjeta Cineco
+  numCompradores: number =0;
 
-  actualizarMaxBoletas(): void {
-    this.maxBoletasCompradas = Math.min(7, this.numBoletas);
-  }
 
   ngOnInit(): void {
     this.nombrePersona = '';
     this.numBoletas = 0;
-    this.tarjetaCineco = true;
-
-    
-
+    this.tarjetaCineco = false;
   }
 
-  procesarCompra(): void {
-    const precioPorBoleta = 12;
-    let descuento = 0;
-  
-    if (this.numBoletas > 5) {
-      descuento = 0.15;
+  procesarCompra() {
+    if (this.numBoletas / this.numCompradores > 7)  {
+      alert(`No puedes comprar más de 7 boletas.`);
+    }
+    let precio = this.numBoletas * this.precioPorBoleta;
+
+    if (this.numBoletas >= 5) {
+      precio *= (1 - 0.15); // Descuento del 15% para más de 5 boletos
     } else if (this.numBoletas >= 3) {
-      descuento = 0.1;
+      precio *= (1 - 0.10); // Descuento del 10% para 3, 4 o 5 boletos
     }
-  
+
     if (this.tarjetaCineco) {
-      descuento += 0.1;
+      precio *= (1 - this.descuentoAdicionalTarjeta); // Descuento adicional del 10% por tarjeta Cineco
     }
-  
-    const precioTotal = this.numBoletas * precioPorBoleta;
-    const descuentoTotal = precioTotal * descuento;
-    this.precioAPagar = precioTotal - descuentoTotal;
-    this.nombrePersona = this.nombrePersona
-    this.maxBoletasCompradas = Math.min(7, this.numBoletas);
+
+    this.precioAPagar = precio / 100; // Convertir el precio a pesos
   }
 }
